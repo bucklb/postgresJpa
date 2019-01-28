1d3f65da0cebe5d6975342744b4a75994b995ead
bucklb <bucklb@hotmail.com>
2019-01-28 23:55:45 +0000

    Profile H2 will populate & start H2, whereas POSTGRES connects to postgres instance on 5432

--- src/main/resources/data-H2.sql
+++ src/main/resources/data-H2.sql
@@ -1,6 +1,6 @@
 
 
--- Just add a question and some corresponding answers.  NEED something in here, else fails to start
+-- Just add a question and some corresponding answers
 
 insert into questions (id, created_at, updated_at, title, description) values (99, now(),now(),  '9', '9');
 insert into answers (id, created_at, updated_at, text, question_id) values (01, now(),now(),  'answer 01', 99);
