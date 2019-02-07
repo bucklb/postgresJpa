24010db858505a798dfba6eb206f2d684503577f
bucklb <bucklb@hotmail.com>
2019-02-07 22:29:36 +0000

    refactored.  Return details of the enrichment that got applied

--- src/main/java/com/example/postgresdemo/controller/BirthController.java
+++ src/main/java/com/example/postgresdemo/controller/BirthController.java
@@ -130,19 +130,16 @@ public class BirthController implements BirthCasesApi {
             BirthCase birth = modelMapper.map(optionalBirthCaseEntity.get(), BirthCase.class);
             List<EnrichmentEntity> enrichments = enrichmentRepository.findByBirthId( birthCaseId );
 
-            // Spin through the list and dump to screen.  There IS a more elegant way ...
+            // Work what we have in database to the desired response (and dump to console)
+            BirthCaseEnrichment bce=enrichmentEntityListAsBirthCaseEnrichment(enrichments);
+
             System.out.println("=== SUBMISSION ==========================================");
             System.out.println(birth.toString());
-//            for(EnrichmentEntity enrichment:enrichments){
-//                System.out.println( enrichment.toString() );
-//                System.out.println(  enrichment.getId() + " council = " + enrichment.getCouncil() + "    org = " + enrichment.getOrganisation() + "  response = " + enrichment.getResponse());
-//            }
-            BirthCaseEnrichment bce=enrichmentEntityListAsBirthCaseEnrichment(enrichments);
             System.out.println(bce.toString());
-
 
             status.setStatus("Submitted");
             httpStatus = HttpStatus.OK;
+
         } else {
 
             status.setStatus("Not found");
@@ -151,6 +148,56 @@ public class BirthController implements BirthCasesApi {
 
         // Pass something back
         return new ResponseEntity<BirthCaseStatus>(status, httpStatus);
+    }
+
+
+    /**
+     * Create an enrichment for the birth.  Yaml has no payload though ...
+     * @param birthCaseId
+     * @return
+     */
+    @Override
+    public  ResponseEntity<BirthCaseEnrichment> birthCasesBirthCaseIdEnrichmentPost(
+            @ApiParam(value = "ID of the Birth Case to return",required=true)
+            @PathVariable("birthCaseId") Long birthCaseId) {
+
+        HttpStatus httpStatus=null;
+        BirthCaseEnrichment bce=null;
+
+        // Generate a fake enrichment pending getting a payload (needs YAML update)
+        bce = createDummyBirthCaseEnrichment();
+
+
+        // Will need the birthCase to add the enrichment
+        Optional<BirthCaseEntity> optionalBirthCaseEntity = birthRepository.findById( birthCaseId );
+        if( optionalBirthCaseEntity.isPresent() ) {
+
+            // Grab the birth entity (complete with its id) as we need it to be in each EnrichmentEntity
+            BirthCaseEntity birthCaseEntity = optionalBirthCaseEntity.get();
+
+            // Convert to the entity form we need
+            List<EnrichmentEntity> entities = enrichmentEntities(bce);
+            for( EnrichmentEntity e : entities ) {
+//                System.out.println(e.toString());
+                e.setBirthCaseEntity(birthCaseEntity);
+                enrichmentRepository.save(e);
+            }
+
+            // Want something to hand back.  For now return the entity list melded back to an enrichment
+            bce = enrichmentEntityListAsBirthCaseEnrichment( entities );
+            httpStatus = HttpStatus.OK;
+
+            // Retrieve the added enrichment
+
+        } else {
+            bce=new BirthCaseEnrichment(); bce.setCouncil("Not found");
+            httpStatus = HttpStatus.NOT_FOUND;
+        }
+
+
+        // Pass something back
+        return new ResponseEntity<BirthCaseEnrichment>(bce, httpStatus);
+
     }
 
     /**
@@ -166,9 +213,13 @@ public class BirthController implements BirthCasesApi {
             bce.setCouncil(entities.get(0).getCouncil());
             for(EnrichmentEntity enrichment:entities){
 
+                boolean b = !(enrichment.getResponse()==null
+                        || enrichment.getResponse().equals("")
+                        || enrichment.getResponse().equals("FALSE"));
+
                 OrganisationsToInformResponse r=new OrganisationsToInformResponse();
                 r.setOrganisation(enrichment.getOrganisation());
-                r.setResponse(enrichment.getResponse()==null || enrichment.getResponse().equals(""));
+                r.setResponse(b);
                 bce.addOrganisationsToInformItem(r);
 
             }
@@ -178,10 +229,10 @@ public class BirthController implements BirthCasesApi {
     }
 
     /**
-     * generate a dummy value
+     * generate a dummy OrganisationsToInformResponse
      * @return
      */
-    private OrganisationsToInformResponse dummyOTIR(String org, boolean b){
+    private OrganisationsToInformResponse createOTIR(String org, boolean b){
         OrganisationsToInformResponse otir=new OrganisationsToInformResponse();
         otir.setOrganisation(org);
         otir.setResponse(b);
@@ -195,7 +246,7 @@ public class BirthController implements BirthCasesApi {
      * @param b
      * @return
      */
-    private EnrichmentEntity enrichmentEntity(String cncl, String org, boolean b){
+    private EnrichmentEntity createEnrichmentEntity(String cncl, String org, boolean b){
         EnrichmentEntity entity = new EnrichmentEntity();
         entity.setCouncil(cncl);
         entity.setOrganisation(org);
@@ -213,66 +264,28 @@ public class BirthController implements BirthCasesApi {
         ArrayList<EnrichmentEntity> entities = new ArrayList<>();
 
         for(OrganisationsToInformResponse otir : birthCaseEnrichment.getOrganisationsToInform()) {
-            entities.add( enrichmentEntity(birthCaseEnrichment.getCouncil(), otir.getOrganisation(), otir.isResponse() ) );
+            entities.add(
+                    createEnrichmentEntity(birthCaseEnrichment.getCouncil(), otir.getOrganisation(), otir.isResponse() ) );
         }
 
         return entities;
     }
 
-
     /**
-     * Create an enrichment for the birth.  Yaml has no payload though ...
-     * @param birthCaseId
+     * Generate a BirthCaseEnrichment pending allowing one as payload
      * @return
      */
-    @Override
-    public  ResponseEntity<BirthCaseEnrichment> birthCasesBirthCaseIdEnrichmentPost(
-            @ApiParam(value = "ID of the Birth Case to return",required=true)
-            @PathVariable("birthCaseId") Long birthCaseId) {
-
-        HttpStatus httpStatus=null;
-        BirthCaseEnrichment bce=null;
+    private BirthCaseEnrichment createDummyBirthCaseEnrichment(){
 
         // Generate a fake enrichment
-        bce = new BirthCaseEnrichment();
+        BirthCaseEnrichment bce = new BirthCaseEnrichment();
+
         bce.setCouncil("municipal");
-        bce.addOrganisationsToInformItem(dummyOTIR("org1",true));
-        bce.addOrganisationsToInformItem(dummyOTIR("org2",false));
+        bce.addOrganisationsToInformItem(createOTIR("org1",true));
+        bce.addOrganisationsToInformItem(createOTIR("org2",false));
 
-
-        // Will need the birthCase to add the enrichment
-        Optional<BirthCaseEntity> optionalBirthCaseEntity = birthRepository.findById( birthCaseId );
-        if( optionalBirthCaseEntity.isPresent() ) {
-
-            // Grab the birth entity (complete with its id) as we need it to be in each EnrichmentEntity
-            BirthCaseEntity birthCaseEntity = optionalBirthCaseEntity.get();
-
-            // Convert to the entity form we need
-            List<EnrichmentEntity> entities = enrichmentEntities(bce);
-            for( EnrichmentEntity e : entities ) {
-                System.out.println(e.toString());
-                e.setBirthCaseEntity(birthCaseEntity);
-                enrichmentRepository.save(e);
-            }
-
-            // Want something to hand back.
-//            bce=new BirthCaseEnrichment(); bce.setCouncil("ENRICHED");
-            bce = enrichmentEntityListAsBirthCaseEnrichment( entities );
-            httpStatus = HttpStatus.OK;
-
-            // Retrieve the added enrichment
-
-        } else {
-            bce=new BirthCaseEnrichment(); bce.setCouncil("Not found");
-            httpStatus = HttpStatus.NOT_FOUND;
-        }
-
-
-        // Pass something back
-        return new ResponseEntity<BirthCaseEnrichment>(bce, httpStatus);
-
+        return bce;
     }
-
 
 
 
