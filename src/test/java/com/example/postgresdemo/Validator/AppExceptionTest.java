5335c6bda8b687d075a82ff22f53ba5d34a16d9d
bucklb <bucklb@hotmail.com>
2019-04-02 17:59:59 +0100

    illustration of repeated throw and gathering data

--- src/test/java/com/example/postgresdemo/Validator/AppExceptionTest.java
+++ src/test/java/com/example/postgresdemo/Validator/AppExceptionTest.java
@@ -5,12 +5,19 @@ import com.example.postgresdemo.exception.BirthCaseApiException;
 import org.bouncycastle.cms.jcajce.JceKTSKeyTransAuthenticatedRecipient;
 import org.junit.Assert;
 import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
+
+
 
 /*
     Looking to use the application stuff in more serious way ...
  */
 public class AppExceptionTest {
+
+    Logger logger = LoggerFactory.getLogger("playTime");
+
 
     @Test
     public void BirthCaseTest() {
@@ -27,6 +34,44 @@ public class AppExceptionTest {
 
 
     }
+
+
+    @Test
+    public void play() {
+        try{
+            throw new Exception("first");
+        } catch (Exception ex1) {
+
+            try{
+                throw new Exception("second",ex1);
+
+            } catch (Exception ex2) {
+                try{
+                    throw new Exception("third",ex2);
+
+                } catch (Exception ex3) {
+                    try{
+                        throw new Exception("fourth",ex3);
+
+                    } catch (Exception ex4) {
+
+                        System.out.println(ex4.getMessage());
+                        System.out.println(ex4.getStackTrace());
+//                        System.out.println(ex4.getMessage());
+
+                        logger.warn("playing", ex4);
+
+                    }
+
+                }
+
+            }
+
+        }
+    }
+
+
+
 
 
 
